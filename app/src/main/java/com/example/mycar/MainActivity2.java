9521fb1fb849221a221d72a53db686b840b35f92
Shirin <amslern@bzz.ch>
2022-09-28 23:33:24 +0200

    basic trying

---
+++ app/src/main/java/com/example/mycar/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.mycar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity2 extends AppCompatActivity {
+    ImageButton btn02;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn02= (ImageButton) findViewById(R.id.imageButton);
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
