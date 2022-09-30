2b849a358c2e56fd47a3ad1ffb69b6febd6e4643
Phillip <jerebicp@bzz.ch>
2022-09-30 07:51:53 +0200

    some updates

---
+++ app/src/main/java/com/example/mycar/AddCar.java
@@ -0,0 +1,87 @@
+package com.example.mycar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCar extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addcar);
+
+        button=(Button)findViewById(R.id.add_car_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AddCar.this,Profilepage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // This callback is called only when there is a saved instance that is previously saved by using
+    // onSaveInstanceState(). We restore some state in onCreate(), while we can optionally restore
+    // other state here, possibly usable after onStart() has completed.
+    // The savedInstanceState Bundle is same as the one used in onCreate().
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("LIFECYCLE", "onRestoreInstanceState");
+    }
+
+    // invoked when the activity may be temporarily destroyed, save the instance state here
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("LIFECYCLE", "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStart() {
+        // call the superclass method first
+        super.onStart();
+        Log.d("LIFECYCLE", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
+        Log.d("LIFECYCLE", "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        // call the superclass method first
+        super.onResume();
+        Log.d("LIFECYCLE", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        // call the superclass method first
+        super.onPause();
+        Log.d("LIFECYCLE", "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        // call the superclass method first
+        super.onRestart();
+        Log.d("LIFECYCLE", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // call the superclass method first
+        super.onDestroy();
+        Log.d("LIFECYCLE", "onDestroy");
+    }
+}
